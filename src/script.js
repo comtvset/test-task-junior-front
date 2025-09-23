c5df9709ceb05828a06e03483fc928ce0a48e662
Vadzim <49336945+comtvset@users.noreply.github.com>
2025-09-23 14:43:04 +0200

    Feat/fetch data (#3)
    * feat: add fetchData(), render()
    
    * feat: add check for missing data

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
+import fetchData from './utils/fetch.js';
+import render from './utils/render.js';
+
+const data = await fetchData();
+render(data);
+
 const body = document.querySelector('body');
 const greeting = document.querySelector('.greeting');
 

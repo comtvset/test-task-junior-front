bd910bea865895b8461911fdab508e6965ca7248
Vadzim <49336945+comtvset@users.noreply.github.com>
2025-09-23 13:16:08 +0200

    feat: add favicon, icons, fonts, main section (#2)

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-console.log('successful connection');
+const body = document.querySelector('body');
+const greeting = document.querySelector('.greeting');
+
+const main = document.querySelector('.main-content');
+
+setTimeout(() => {
+  body.removeChild(greeting);
+  main.classList.add('show');
+}, 3500);
